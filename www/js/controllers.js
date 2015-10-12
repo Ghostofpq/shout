e5538cfbed8d34b6c9d9dc0ea7b7d62dd350bc25
VMPX4526 <mathieu1.perez@orange.com>
2015-10-12 16:36:03 +0200

    ok build phone

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function ($scope) {})
 
-.controller('ChatCtrl', function ($scope, $ionicScrollDelegate, Chat) {
+.controller('ChatCtrl', function ($scope, $ionicScrollDelegate, Chat, $interval) {
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
     // To listen for when this page is active (for example, to refresh data),
@@ -10,9 +10,11 @@ angular.module('starter.controllers', [])
     //
     //$scope.$on('$ionicView.enter', function(e) {
     //});
-
+    $scope.watch = {};
+    $scope.temporal = {};
+    $scope.pos = {}
     $scope.chat = [];
-    $scope.position = Chat.getPosition();
+
     $scope.message = "";
 
     $scope.publish = function () {
@@ -20,9 +22,17 @@ angular.module('starter.controllers', [])
         $scope.message = "";
     };
     $scope.$on('newMessage', function (event, args) {
-        console.log(args);
         $scope.chat.unshift(args);
         $ionicScrollDelegate.scrollTop();
+    });
+
+    $scope.watch = navigator.geolocation.watchPosition(function (updatedPos) {
+        $scope.pos = updatedPos;
+        console.log(updatedPos);
+        Chat.refreshPosition(updatedPos);
+    }, function (error) {
+        console.log("Watch error!");
+        console.log(error);
     });
 })
 
@@ -35,6 +45,44 @@ angular.module('starter.controllers', [])
     $scope.setColor = function () {
         Chat.setColor($scope.color);
     };
+})
+
+.directive('input', function ($timeout) {
+    return {
+        restrict: 'E',
+        scope: {
+            'returnClose': '=',
+            'onReturn': '&',
+            'onFocus': '&',
+            'onBlur': '&'
+        },
+        link: function (scope, element, attr) {
+            element.bind('focus', function (e) {
+                if (scope.onFocus) {
+                    $timeout(function () {
+                        scope.onFocus();
+                    });
+                }
+            });
+            element.bind('blur', function (e) {
+                if (scope.onBlur) {
+                    $timeout(function () {
+                        scope.onBlur();
+                    });
+                }
+            });
+            element.bind('keydown', function (e) {
+                if (e.which == 13) {
+                    if (scope.returnClose) element[0].blur();
+                    if (scope.onReturn) {
+                        $timeout(function () {
+                            scope.onReturn();
+                        });
+                    }
+                }
+            });
+        }
+    }
 });
 
 //.controller('LoadingUpCtrl', function ($cordovaGeolocation) {
--- www/js/services.js
+++ www/js/services.js
@@ -2,39 +2,35 @@ angular.module('starter.services', ['firebase'])
 
 .factory('Chat', function ($rootScope) {
     var content = []; //  to keep track of what is received
-   
+
     var fluxCenter;
     var fluxNorth;
     var fluxSouth;
     var fluxEast;
     var fluxWest;
-
+    var position;
     var lastTs = Date.now() - 60 * 1000 * 2;
 
-    var position = {
-        'coords.latitude': 45.745125,
-        'coords.longitude': 4.861638
-    };
     var settings = {
         'textColor': "black",
         'backgroundColor': "white",
         'name': "anon"
     };
     // Might use a resource here that returns a JSON array
-    var center = function () {
-        return (Math.round(position["coords.latitude"] * 1000)) + "x" + (Math.round(position["coords.longitude"] * 1000));
+    var center = function (pos) {
+        return (Math.round(pos.coords.latitude * 1000)) + "x" + (Math.round(pos.coords.longitude * 1000));
     }
-    var north = function () {
-        return ((Math.round(position["coords.latitude"] * 1000) + 1)) + "x" + (Math.round(position["coords.longitude"] * 1000));
+    var north = function (pos) {
+        return ((Math.round(pos.coords.latitude * 1000) + 1)) + "x" + (Math.round(pos.coords.longitude * 1000));
     }
-    var south = function () {
-        return ((Math.round(position["coords.latitude"] * 1000) - 1)) + "x" + (Math.round(position["coords.longitude"] * 1000));
+    var south = function (pos) {
+        return ((Math.round(pos.coords.latitude * 1000) - 1)) + "x" + (Math.round(pos.coords.longitude * 1000));
     }
-    var east = function () {
-        return (Math.round(position["coords.latitude"] * 1000)) + "x" + ((Math.round(position["coords.latitude"] * 1000) - 1));
+    var east = function (pos) {
+        return (Math.round(pos.coords.latitude * 1000)) + "x" + ((Math.round(pos.coords.longitude * 1000) - 1));
     }
-    var west = function () {
-        return (Math.round(position["coords.latitude"] * 1000)) + "x" + ((Math.round(position["coords.latitude"] * 1000) + 1));
+    var west = function (pos) {
+        return (Math.round(pos.coords.latitude * 1000)) + "x" + ((Math.round(pos.coords.longitude * 1000) + 1));
     }
 
     var receive = function (payload) {
@@ -42,16 +38,17 @@ angular.module('starter.services', ['firebase'])
             console.log("received : " + payload.m);
             content[payload.n + payload.ts] = payload;
             lastTs = payload.ts;
-            $rootScope.$broadcast("newMessage",payload);
+            $rootScope.$broadcast("newMessage", payload);
         }
     }
 
     var refreshFluxes = function () {
-        fluxCenter = new Firebase("https://vivid-heat-5271.firebaseio.com/shout/" + center());
-        fluxNorth = new Firebase("https://vivid-heat-5271.firebaseio.com/shout/" + north());
-        fluxSouth = new Firebase("https://vivid-heat-5271.firebaseio.com/shout/" + south());
-        fluxEast = new Firebase("https://vivid-heat-5271.firebaseio.com/shout/" + east());
-        fluxWest = new Firebase("https://vivid-heat-5271.firebaseio.com/shout/" + west());
+        console.log("Opening fluxes for " + center(position));
+        fluxCenter = new Firebase("https://vivid-heat-5271.firebaseio.com/shout/" + center(position));
+        fluxNorth = new Firebase("https://vivid-heat-5271.firebaseio.com/shout/" + north(position));
+        fluxSouth = new Firebase("https://vivid-heat-5271.firebaseio.com/shout/" + south(position));
+        fluxEast = new Firebase("https://vivid-heat-5271.firebaseio.com/shout/" + east(position));
+        fluxWest = new Firebase("https://vivid-heat-5271.firebaseio.com/shout/" + west(position));
         fluxCenter.on('child_added', function (snapshot) {
             receive(snapshot.val());
         });
@@ -68,7 +65,6 @@ angular.module('starter.services', ['firebase'])
             receive(snapshot.val());
         });
     }
-    refreshFluxes();
 
     var shout = function (message) {
         var payload = {
@@ -76,7 +72,7 @@ angular.module('starter.services', ['firebase'])
             'n': settings.name,
             'tc': settings.textColor,
             'bc': settings.backgroundColor,
-            'p': position["coords.latitude"] + "x" + position["coords.latitude"],
+            'p': center(position),
             'm': message
         }
         fluxCenter.push(payload);
@@ -87,16 +83,14 @@ angular.module('starter.services', ['firebase'])
     }
 
     return {
-        
-        forcePosition: function (newPosition) {
-            position = newPosition;
-            refreshFluxes();
-        },
         getPosition: function () {
             return position;
         },
-        refreshPosition: function () {
-            refreshFluxes();
+        refreshPosition: function (newPosition) {
+            if (position == null || (center(position) != center(newPosition))) {
+                position = newPosition;
+                refreshFluxes();
+            }
             return this.getPosition();
         },
         publish: function (message) {
